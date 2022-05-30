bf493ab113ad7013f18d403d6f6889254ba73454
sametavcii <sametavc05@gmail.com>
2022-05-30 20:33:25 +0300

    Database migration version 2 added

--- db/migration/000002_add_users.down.sql
+++ db/migration/000002_add_users.down.sql
@@ -1,8 +1,3 @@
-CREATE TABLE "users" (
-                         "username" varchar PRIMARY KEY,
-                         "hashed_password" varchar NOT NULL,
-                         "full_name" varchar NOT NULL,
-                         "email" varchar UNIQUE NOT NULL,
-                         "password_changed_at" timestamptz NOT NULL DEFAULT '0001-01-01 00:00:00Z',
-                         "created_at" timestamptz NOT NULL DEFAULT 'now()'
-);
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "owner_currency_key";
+ALTER TABLE IF EXISTS "accounts" DROP CONSTRAINT IF EXISTS "accounts_owner_fkey";
+DROP TABLE IF EXISTS "users"
