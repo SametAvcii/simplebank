2e23322e613c56fba8f36a9d9b4e339ed04c4a03
sametavcii <sametavc05@gmail.com>
2022-05-31 16:49:34 +0300

    Add users file and test

--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -11,8 +11,9 @@ import (
 )
 
 func createRandomAccount(t *testing.T) Accounts {
+	user := createRandomUser(t)
 	arg := CreateAccountParams{
-		Owner:    util.RandomOwner(),
+		Owner:    user.Username,
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
